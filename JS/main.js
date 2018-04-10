d0ace69cfb46853634c0c0a5aaebc203acb8c6fe
Shiva Prasad <as.prasad50@gmail.com>
2018-04-10 10:35:35 +0530

    JS-1

---
+++ JS/main.js
@@ -0,0 +1,38 @@
+console.log( 10 - 10);
+// alert( 10 * 10);
+
+// document.write();
+/* DataTypes....*/
+
+
+// variables 
+var x,y;
+
+/*x = 10;
+y = 10;
+console.log(x + y);
+
+
+// x = 20;
+// y = 30;
+console.log(x - y);
+
+
+x = "40.23121231212";
+y = 40
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+*/
+// strings, numbers, boolean(true/false), null, undefined
+//  array, Object, functions,
+
+
+var name = "Shiva's Class";
+var num = 10.34234;
+var yes = false;
+
+var Nul = null;
+var Und;
+
+console.log();
